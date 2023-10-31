561a43774f44393956ec1288d0954fc95abcf267
AnnikaWilbrink <awilbrink23@gmail.com>
2023-10-31 10:05:29 +0100

    opschonen frontend

---
+++ script.js
@@ -0,0 +1,91 @@
+async function addFooter() {
+    const resp = await fetch("footer.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforeend", html);
+}   
+addFooter()
+
+async function addNav() {
+    let userRole = localStorage.getItem("WT_ROLE");
+    if (userRole == "admin") {
+        const resp = await fetch("nav-admin.html");
+        const html = await resp.text();
+        document.body.insertAdjacentHTML("afterbegin", html);
+
+        const currentPage = location.pathname.split("/").slice(-1)[0].replace(".html", "");
+
+        const pageToLinkMap = {
+        "admin-home": "home-link",
+        "boeken-overzicht": "book-link",
+        "boek-pagina": "book-link",
+        "admin-statistieken": "stats-link",
+        "admin-book-management": "manage-link",
+        "admin-book-copy-management": "manage-link",
+        "admin-append-book": "manage-link",
+        "admin-account": "account-link",
+        "favorites": "fav-link",
+        "admin-users": "users-link",
+        };
+
+        const linkId = pageToLinkMap[currentPage];
+        if (linkId) {
+            const link = document.getElementById(linkId);
+            if (link) {
+                link.classList.add("active");
+            }
+        }
+    } else if (userRole == "user") {
+        const resp = await fetch("nav-user.html");
+        const html = await resp.text();
+        document.body.insertAdjacentHTML("afterbegin", html);
+
+        // Get the current page's filename (e.g., user-home.html) and extract the part before ".html"
+        const currentPage = location.pathname.split("/").slice(-1)[0].replace(".html", "");
+
+        // Define an object mapping page names to the corresponding link IDs
+        const pageToLinkMap = {
+        "user-home": "home-link",
+        "boeken-overzicht": "book-link",
+        "boek-pagina": "book-link",
+        "user-account": "account-link",
+        "favorites": "fav-link",
+        };
+
+        // Find the corresponding link by ID and add the "active" class to it
+        const linkId = pageToLinkMap[currentPage];
+        if (linkId) {
+            const link = document.getElementById(linkId);
+            if (link) {
+                link.classList.add("active");
+            }
+        }
+
+    } else if (userRole == "front-desk") {
+        const resp = await fetch("nav-frdesk.html");
+        const html = await resp.text();
+        document.body.insertAdjacentHTML("afterbegin", html);
+
+        // Get the current page's filename (e.g., user-home.html) and extract the part before ".html"
+        const currentPage = location.pathname.split("/").slice(-1)[0].replace(".html", "");
+
+        // Define an object mapping page names to the corresponding link IDs
+        const pageToLinkMap = {
+        "frdesk-home": "home-link",
+        "boeken-overzicht": "book-link",
+        "boek-pagina": "book-link",
+        "frdesk-account": "account-link",
+        "favorites": "fav-link",
+        };
+
+        // Find the corresponding link by ID and add the "active" class to it
+        const linkId = pageToLinkMap[currentPage];
+        if (linkId) {
+            const link = document.getElementById(linkId);
+            if (link) {
+                link.classList.add("active");
+            }
+        }
+    }
+        
+}
+addNav()
